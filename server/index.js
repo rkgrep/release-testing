262936bf50e1b444949be637f44181559ef5d5ea
Roman Kinyakin <1@grep.su>
2017-06-24 01:48:23 +0700

    Remove comments

--- server/index.js
+++ server/index.js
@@ -14,14 +14,10 @@ app.use('/api', api)
 
 // Start nuxt.js
 async function start() {
-  // Import and Set Nuxt.js options
   let config = require('../nuxt.config.js')
   config.dev = !(process.env.NODE_ENV === 'production')
-  // Instanciate nuxt.js
   const nuxt = new Nuxt(config)
-  // Add nuxt.js middleware
   app.use(nuxt.render)
-  // Listen the server
   app.listen(port, host)
   console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
 }
